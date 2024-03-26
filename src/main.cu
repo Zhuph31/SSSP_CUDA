2d62076ae706f61a79f023c80c81dbdf1eb0d6e2
Shi  Yiyang <shiyiya1@ug77.eecg.toronto.edu>
2024-03-25 20:36:49 -0400

    add cooperative blocking

--- src/main.cu
+++ src/main.cu
@@ -24,6 +24,68 @@ bool compare(std::vector<edge_data_type>& cpu, edge_data_type* gpu) {
     return true;
 }
 
+void init_trivial(CSRGraph& g) {
+    g.nnodes = 6;
+    g.nedges = 16;
+    g.row_start = (index_type *)malloc((g.nnodes+1)*sizeof(index_type));
+    g.edge_dst = (index_type *)malloc(g.nedges*sizeof(edge_data_type));
+    g.edge_data = (edge_data_type *)malloc(g.nedges*sizeof(edge_data_type));
+    g.node_data = (node_data_type *)malloc(g.nnodes*sizeof(edge_data_type));
+
+    g.row_start[0] = 0;
+    g.row_start[1] = 2;
+    g.row_start[2] = 5;
+    g.row_start[3] = 9;
+    g.row_start[4] = 11;
+    g.row_start[5] = 13;
+    g.row_start[6] = 16;
+
+    g.edge_dst[0] = 1;
+    g.edge_dst[1] = 2;
+
+    g.edge_dst[2] = 0;
+    g.edge_dst[3] = 2;
+    g.edge_dst[4] = 3;
+
+    g.edge_dst[5] = 0;
+    g.edge_dst[6] = 1;
+    g.edge_dst[7] = 4;
+    g.edge_dst[8] = 5;
+
+    g.edge_dst[9] = 1;
+    g.edge_dst[10] = 5;
+
+    g.edge_dst[11] = 2;
+    g.edge_dst[12] = 5;
+
+    g.edge_dst[13] = 2;
+    g.edge_dst[14] = 3;
+    g.edge_dst[15] = 4;
+
+    g.edge_data[0] = 2;
+    g.edge_data[1] = 4;
+
+    g.edge_data[2] = 2;
+    g.edge_data[3] = 1;
+    g.edge_data[4] = 6;
+
+    g.edge_data[5] = 4;
+    g.edge_data[6] = 1;
+    g.edge_data[7] = 2;
+    g.edge_data[8] = 3;
+
+    g.edge_data[9] = 6;
+    g.edge_data[10] = 5;
+
+    g.edge_data[11] = 2;
+    g.edge_data[12] = 4;
+
+    g.edge_data[13] = 3;
+    g.edge_data[14] = 5;
+    g.edge_data[15] = 4;
+}
+
+
 
 
 int main(int argc, char** argv) {
@@ -37,7 +99,7 @@ int main(int argc, char** argv) {
 
     g.read(argv[1]); 
     //g.read("inputs/rmat22.gr");
-    // init_trivial_graph(g);
+    //init_trivial_graph(g);
 
 
     std::vector<edge_data_type> out_cpu;
@@ -53,8 +115,8 @@ int main(int argc, char** argv) {
     check_cuda(cudaMallocHost(&h_d, g.nnodes * sizeof(edge_data_type),cudaHostAllocWriteCombined));
 
     start = getTimeStamp();
-    //workfront_sweep(g, h_d);
-    nearfar(g,h_d);
+    workfront_sweep(g, h_d);
+    //nearfar(g,h_d);
     end = getTimeStamp();
     double gpu_time = end - start;
     printf("Total GPU time: %f\n",gpu_time);
--- src/workfront-sweep.cu
+++ src/workfront-sweep.cu
@@ -29,6 +29,141 @@ __global__ void wf_iter(CSRGraph g, edge_data_type* d, index_type* last_q, index
     }
 }
 
+__global__ void wf_coop_iter_impl1(CSRGraph g, edge_data_type* d, index_type* last_q, index_type last_q_len, index_type* new_q, index_type* pq_idx,  index_type* scratch) {
+    index_type index = threadIdx.x + (blockDim.x * blockIdx.x);
+
+    __shared__ index_type block_row_start;
+    __shared__ index_type block_row_end; 
+    __shared__ index_type s_idx; 
+
+    //decide what each block work range is 
+    index_type block_index_start = blockIdx.x * blockDim.x;
+    index_type block_index_end = block_index_start + blockDim.x;  
+    //do not go beyond last_q_len
+    block_index_end = min(block_index_end, last_q_len); 
+
+    for (index_type index=block_index_start; index < block_index_end; index++){
+        //only first thread in block load row start and end and source index 
+        if (threadIdx.x == 0){
+            s_idx = last_q[index];
+            block_row_start = g.row_start[s_idx]; 
+            block_row_end = g.row_start[s_idx + 1]; 
+            //printf("source id %d, block_row_start %d, block_row_end %d \n", s_idx, block_row_start, block_row_end);
+        }
+        __syncthreads();
+
+        //the threads in this block each take one edge
+        for (index_type j =threadIdx.x + block_row_start; j < block_row_end; j += blockDim.x){
+            
+            edge_data_type w = d[s_idx];
+            edge_data_type ew = g.edge_data[j];
+            index_type n = g.edge_dst[j];
+            edge_data_type nw = d[n];
+            edge_data_type new_w = ew + w;
+            // Check if the distance is already set to max then just take the max since,
+            if (w >= MAX_VAL){
+                new_w = MAX_VAL;
+            }
+
+            if (new_w < nw) {
+                atomicMin(&d[n],new_w);
+                if (atomicCAS(&scratch[n],0,index) == 0) {
+                    index_type q_idx = atomicAdd(pq_idx,1);
+                    new_q[q_idx] = n;
+                }
+            }
+        }
+
+    }
+    
+}
+
+
+__device__ index_type bisect_left(index_type *block, index_type lo, index_type hi, index_type value){
+    index_type mid;
+    while (lo < hi){
+        mid = lo + (hi - lo) / 2; 
+        if (block[mid] < value)
+            lo = mid + 1;
+        else
+            hi = mid; 
+    }
+    
+    return (lo > 0) ? lo - 1 : lo; 
+}
+
+
+
+
+
+#define BLOCK_DIM_X 128
+__global__ void wf_coop_iter_impl2(CSRGraph g, edge_data_type* d, index_type* last_q, index_type last_q_len, index_type* new_q, index_type* pq_idx,  index_type* scratch) {
+    
+    //one for start, one for end.
+    __shared__ index_type block_row_offset[BLOCK_DIM_X * 2];
+    index_type index = threadIdx.x + (blockDim.x * blockIdx.x);
+
+    //decide what each block work range is 
+    index_type block_index_start = blockIdx.x * blockDim.x;
+    index_type block_index_end = block_index_start + blockDim.x;  
+    index_type s_idx; 
+    //do not go beyond last_q_len
+    block_index_end = min(block_index_end, last_q_len); 
+    __shared__ int max_index; 
+    max_index = -1;
+
+    if (index < block_index_end){
+        //load start and end offset into shared memory
+        s_idx = last_q[index];
+        block_row_offset[threadIdx.x * 2] = g.row_start[s_idx];
+        block_row_offset[threadIdx.x * 2 + 1] = g.row_start[s_idx + 1]; 
+        atomicAdd(&max_index, 2); 
+        printf("index %d, s_idx %d, start %d, end %d, max_index %d\n", index ,s_idx, g.row_start[s_idx], g.row_start[s_idx + 1], max_index);
+    }
+    __syncthreads();
+
+
+    index_type row_offset = threadIdx.x + block_row_offset[0];
+    index_type block_max = block_row_offset[max_index];
+
+    //all thread work on one item in the offset array 
+    //example offset_array 0, 2, 8, 16, 19, a block have 8 threads
+    //first work on 0, 2, 8, then work on 8, 16, last work on 16, 19
+    while (row_offset < block_max){
+
+        //find source index
+        s_idx = bisect_left(block_row_offset, 0, max_index, row_offset);
+
+        edge_data_type w = d[s_idx];
+        edge_data_type ew = g.edge_data[row_offset];
+        index_type n = g.edge_dst[row_offset];
+        edge_data_type nw = d[n];
+        edge_data_type new_w = ew + w;
+
+        printf("working, row_offset %d, s_idx %d, d_idx %d, new_w %d, edge weight %d, weight %d\n", row_offset, s_idx, n, new_w, ew, w); 
+        // Check if the distance is already set to max then just take the max since,
+        if (w >= MAX_VAL){
+            new_w = MAX_VAL;
+        }
+
+
+        if (new_w < nw) {
+            atomicMin(&d[n],new_w);
+            if (atomicCAS(&scratch[n],0,index) == 0) {
+                index_type q_idx = atomicAdd(pq_idx,1);
+                new_q[q_idx] = n;
+            }
+        }
+
+        row_offset += blockDim.x; 
+    }
+    
+}
+
+
+
+
+
 void workfront_sweep(CSRGraph& g, edge_data_type* dists) {
     double start,end = 0;
     CSRGraph d_g;
@@ -58,7 +193,9 @@ void workfront_sweep(CSRGraph& g, edge_data_type* dists) {
         index_type len = *qlen;
         *qlen = 0;
  
-        wf_iter<<<(len + 512 - 1) / 512,512>>>(d_g, d_d, q1, len,q2, qlen, qscratch);
+        //wf_iter<<<(len + 512 - 1) / 512,512>>>(d_g, d_d, q1, len,q2, qlen, qscratch);
+        wf_coop_iter_impl1<<<(len + 128 - 1) / 128,128>>>(d_g, d_d, q1, len,q2, qlen, qscratch);
+        //wf_coop_iter_impl2<<<(len + 128 - 1) / 128,128>>>(d_g, d_d, q1, len,q2, qlen, qscratch);
         check_cuda(cudaMemset(qscratch,0,g.nnodes*sizeof(index_type)));
         cudaDeviceSynchronize();
 
