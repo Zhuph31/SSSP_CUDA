03849a38d12873bdd650fdd725bc13c016a38c5f
xjjjack <xhaoyiqing@gmail.com>
2024-04-10 21:27:24 -0400

    add delta and rho stepping

---
+++ src/Parallel-SSSP/sssp.cc
@@ -0,0 +1,146 @@
+#include "sssp.h"
+
+#include <fstream>
+#include <functional>
+#include <numeric>
+
+#include "dijkstra.h"
+
+using namespace std;
+using namespace parlay;
+
+template <class Algo>
+void run(Algo &algo, const Graph &G, bool verify) {
+  for (int v = 0; v < 1; v++) {
+    NodeId s = 0;
+    printf("source %d: %-10d\n", v, s);
+    double total_time = 0;
+    for (int i = 0; i <= NUM_ROUND; i++) {
+      internal::timer t;
+      algo.sssp(s);
+      t.stop();
+      if (i == 0) {
+        printf("Warmup Round: %f\n", t.total_time());
+      } else {
+        printf("Round %d: %f\n", i, t.total_time());
+        total_time += t.total_time();
+      }
+    }
+    double average_time = total_time / NUM_ROUND;
+    printf("Average time: %f\n", average_time);
+
+    ofstream ofs("sssp.tsv", ios_base::app);
+    ofs << average_time << '\t';
+    ofs.close();
+
+    if (verify) {
+      printf("Running verifier...\n");
+      internal::timer t;
+      auto dist = algo.sssp(s);
+      t.stop();
+      printf("Our running time: %f\n", t.total_time());
+      verifier(s, G, dist);
+    }
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    fprintf(
+        stderr,
+        "Usage: %s [-i input_file] [-p parameter] [-w] [-s] [-v] [-a "
+        "algorithm]\n"
+        "Options:\n"
+        "\t-i,\tinput file path\n"
+        "\t-p,\tparameter(e.g. delta, rho)\n"
+        "\t-w,\tweighted input graph\n"
+        "\t-s,\tsymmetrized input graph\n"
+        "\t-v,\tverify result\n"
+        "\t-a,\talgorithm: [rho-stepping] [delta-stepping] [bellman-ford]\n",
+        argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  char c;
+  bool weighted = false;
+  bool symmetrized = false;
+  bool verify = false;
+  string param;
+  int algo = rho_stepping;
+  char const *FILEPATH = nullptr;
+  while ((c = getopt(argc, argv, "i:p:a:wsv")) != -1) {
+    switch (c) {
+      case 'i':
+        FILEPATH = optarg;
+        break;
+      case 'p':
+        param = string(optarg);
+        break;
+      case 'a':
+        if (!strcmp(optarg, "rho-stepping")) {
+          algo = rho_stepping;
+        } else if (!strcmp(optarg, "delta-stepping")) {
+          algo = delta_stepping;
+        } else if (!strcmp(optarg, "bellman-ford")) {
+          algo = bellman_ford;
+        } else {
+          fprintf(stderr, "Error: Unknown algorithm %s\n", optarg);
+          exit(EXIT_FAILURE);
+        }
+        break;
+      case 'w':
+        weighted = true;
+        break;
+      case 's':
+        symmetrized = true;
+        break;
+      case 'v':
+        verify = true;
+        break;
+      default:
+        fprintf(stderr, "Error: Unknown option %c\n", optopt);
+        exit(EXIT_FAILURE);
+    }
+  }
+  Graph G(weighted, symmetrized);
+
+  printf("Reading graph...\n");
+  G.read_graph(FILEPATH);
+  if (!weighted) {
+    printf("Generating edge weights...\n");
+    G.generate_weight();
+  }
+
+  fprintf(stdout,
+          "Running on %s: |V|=%zu, |E|=%zu, param=%s, num_src=%d, "
+          "num_round=%d\n",
+          FILEPATH, G.n, G.m, param.c_str(), NUM_SRC, NUM_ROUND);
+
+  int sd_scale = G.m / G.n;
+  if (algo == rho_stepping) {
+    size_t rho = 1 << 20;
+    if (param != "") {
+      rho = stoull(param);
+    }
+    Rho_Stepping solver(G, rho);
+    solver.set_sd_scale(sd_scale);
+    run(solver, G, verify);
+  } else if (algo == delta_stepping) {
+    EdgeTy delta = 1 << 15;
+    if (param != "") {
+      if constexpr (is_integral_v<EdgeTy>) {
+        delta = stoull(param);
+      } else {
+        delta = stod(param);
+      }
+    }
+    Delta_Stepping solver(G, delta);
+    solver.set_sd_scale(sd_scale);
+    run(solver, G, verify);
+  } else if (algo == bellman_ford) {
+    Bellman_Ford solver(G);
+    solver.set_sd_scale(sd_scale);
+    run(solver, G, verify);
+  }
+  return 0;
+}
